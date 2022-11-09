89fdd72ab02bf2796fa26cffe9feef0149434a00
unknown <chadiegilaugis@gmail.com>
2022-11-09 20:30:54 +0800

    update controller

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,34 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const Workout = require("../models/workoutModel");
-router.get("/", (req, res) => {
-  res.json({ mgs: "get all workouts" });
-});
 
-router.get("/:id", (req, res) => {
-  res.json({ msg: "get single workout" });
-});
+const {
+  createWorkout,
+  getAllWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
 
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-  try {
-    const workout = await Workout.create({
-      title,
-      load,
-      reps,
-    });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.get("/", getAllWorkouts);
 
-router.delete("/:id", (req, res) => {
-  res.json({ msg: "delete a new workout" });
-});
+router.get("/:id", getWorkout);
 
-router.patch("/:id", (req, res) => {
-  res.json({ msg: "update a new workout" });
-});
+router.post("/", createWorkout);
+
+router.delete("/:id", deleteWorkout);
+
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
